d395606344454be787eb8ff9362bec9861600702
Aakash Nandrajog <nandrajog.aakash@yahoo.com>
2025-04-20 15:59:10 +0200

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
 // Define function
 def call(String ProjectName, String ImageTag, String DockerHubUser){
-   docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} .
+   "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
