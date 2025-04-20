e5923cea66515d1ffae6f470dd6cd9a01e2fc7ea
Aakash Nandrajog <nandrajog.aakash@yahoo.com>
2025-04-20 15:12:03 +0200

    Update sonarqube_code_quality.groovy

--- vars/sonarqube_code_quality.groovy
+++ vars/sonarqube_code_quality.groovy
@@ -1,5 +1,5 @@
-def call(){
-  timeout(time: 1, unit: "MINUTES"){
-      waitForQualityGate abortPipeline: false
-  }
+def call() {
+    timeout(time: 1, unit: "MINUTES") {
+        waitForQualityGate abortPipeline: false, scannerContextPath: '.scanner/report-task.txt'
+    }
 }
